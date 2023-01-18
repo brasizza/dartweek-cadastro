4b4ad2dff14222b96446c239fdda7cbaa6c677a7
Marcus Vinicius Brasizza <mvbdesenvolvimento@gmail.com>
2023-01-18 19:34:55 -0300

    res

--- resources/views/concluido/index.blade.php
+++ resources/views/concluido/index.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dart Week - 2023</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,300;0,400;0,700;1,400;1,700" display="swap"
         rel="stylesheet">
