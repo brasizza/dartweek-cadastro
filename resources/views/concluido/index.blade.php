29f4fb3c3f2e0293c413157cd75d483611544712
brasizza <mvbdesenvolvimento@gmail.com>
2024-02-05 18:00:42 -0300

    certificado

--- resources/views/concluido/index.blade.php
+++ resources/views/concluido/index.blade.php
@@ -23,15 +23,13 @@
             <span>Parabéns! Você está recebendo o seu Certificado por ter participado e concluído a <strong>Flutter experience!</strong></span><br>
 
             <span>Esperamos fortemente que este seja apenas seu primeiro certificado,<br>e que você possa continuar
-                conosco nesta próxima turma da <strong>Academia do Flutter</strong>,<br>aprendendo desde o nível
+                conosco nesta próxima turma da <strong><a href="https://academiadoflutter.com.br" style="color: black"  target="_blank">Academia do Flutter</a></strong>,<br>aprendendo desde o nível
                 Iniciante até o Avançado, continuando a usufruir da minha mentoria e se capacitando para o mercado de
                 trabalho,<br><strong>que cresce a cada dia mais!</strong></span>
             <span>
                 <h3>Vem conhecer o Melhor e mais Completo curso de Dart e Flutter do Brasil</h3>
-                <h3 class="button"><a href="https://academiadoflutter.com.br"    style="color: white"  target="_blank">https://academiadoflutter.com.br</a>
-
             </span>
-
+            <br>
 
             <span><strong>Conte conosco!</strong></span><br>
             <span>Abraços,</span><br>
